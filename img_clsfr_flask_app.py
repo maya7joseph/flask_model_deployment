dc77111d0c2e37eb9e92e8ccd1a41bb835d4deef
Maya Joseph <73503859+maya7joseph@users.noreply.github.com>
2020-11-01 14:46:04 -0500

    Changed port so it is not hard coded

--- img_clsfr_flask_app.py
+++ img_clsfr_flask_app.py
@@ -48,10 +48,9 @@ def prediction():
           
 
 
-if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port, debug=True)
-    
-
 # if __name__ == '__main__':
-# 	app.run(debug=True)
+# 	port = int(os.environ.get('PORT', 5000))
+# 	app.run(host='0.0.0.0', port=port, debug=True)
+    
+if __name__ == '__main__':
+	app.run(debug=True)
